08b26da1cb9f34d9ef7760c87d5effb16ade9fda
JP-Rockpile <82130034+JP-Rockpile@users.noreply.github.com>
2021-07-30 12:22:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friends for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isCompleted:false}];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    let temp = [...todos];    
+    temp.splice(index, 1);
+    setTodos(temp);
+  }
+
+  return(
+    <div className="app"> <h1> My ToDo List</h1> 
+      <hr></hr> 
+      <p>Type a new list item in the "Add Todo" box. Press enter to add. Click on an item to remove it from the list.</p>
+      <div className="todo-list" >
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
